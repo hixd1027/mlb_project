cfd51ebc90f8183ce88101dc1f5461afe3ce0d8a
hixd1027 <hixd1027@gmail.com>
2026-06-02 10:26:57 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,17 +1,14 @@
 <?php
+session_start(); 
+
 $host = 'localhost';
-$dbname = 'mlb_project';
-$username = 'root'; 
-$password = '';     
+$dbuser = 'ciai_dbst';
+$dbpassword = 'Nptu_123456';
+$dbname = 'cej113028'; 
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $dbuser, $dbpassword);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-    
 } catch(PDOException $e) {
     echo "資料庫連線失敗: " . $e->getMessage();
     exit;
